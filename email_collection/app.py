244575d1cb45753847aba3ce331342bc013562af
bharatkatyal <bharatkatyal@live.com>
2023-12-11 16:25:24 -0500

    working cors for local, s3, postman, writing to db. now for security cleanup

--- email_collection/app.py
+++ email_collection/app.py
@@ -1,3 +1,64 @@
+# import json
+# import boto3
+# import os
+# import logging
+
+# # Configure logging
+# logger = logging.getLogger()
+# logger.setLevel(logging.INFO)
+
+# dynamodb = boto3.resource('dynamodb')
+# table = dynamodb.Table(os.environ['USER_DATA_TABLE'])
+# ses = boto3.client('ses', region_name=os.environ['SES_REGION'])
+# source_email = os.environ['SES_SOURCE_EMAIL']
+
+# def lambda_handler(event, context):
+#     body = json.loads(event['body'])
+    
+#     first_name = body.get('first_name', '')
+#     email = body.get('email', '')
+    
+#     if not first_name or not email:
+#         return {
+#             'statusCode': 400,
+#             'body': json.dumps('Both first_name and email are required.')
+#         }
+    
+#     try:
+#         # Store data in DynamoDB
+#         table.put_item(Item={'email': email, 'first_name': first_name})
+        
+#         # Send an email to the user's provided email address
+#         response = ses.send_email(
+#             Source=source_email,
+#             Destination={
+#                 'ToAddresses': [email]  # Use the user's provided email as the destination
+#             },
+#             Message={
+#                 'Subject': {
+#                     'Data': 'Data Submitted'
+#                 },
+#                 'Body': {
+#                     'Text': {
+#                         'Data': f'First Name: {first_name}\nEmail: {email}'
+#                     }
+#                 }
+#             }
+#         )
+        
+#         return {
+#             'statusCode': 200,
+#             'body': json.dumps('Data submitted successfully. An email has been sent to your provided email address......')
+#         }
+#     except Exception as e:
+#         # Log the error to CloudWatch Logs
+#         logger.error(f'Error: {str(e)}')
+        
+#         return {
+#             'statusCode': 500,
+#             'body': json.dumps(f'Error: {str(e)}')
+#         }
+
 import json
 import boto3
 import os
@@ -21,6 +82,10 @@ def lambda_handler(event, context):
     if not first_name or not email:
         return {
             'statusCode': 400,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json'
+            },
             'body': json.dumps('Both first_name and email are required.')
         }
     
@@ -48,7 +113,11 @@ def lambda_handler(event, context):
         
         return {
             'statusCode': 200,
-            'body': json.dumps('Data submitted successfully. An email has been sent to your provided email address...')
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json'
+            },
+            'body': json.dumps('Data submitted successfully. An email has been sent to your provided email address......')
         }
     except Exception as e:
         # Log the error to CloudWatch Logs
@@ -56,5 +125,9 @@ def lambda_handler(event, context):
         
         return {
             'statusCode': 500,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json'
+            },
             'body': json.dumps(f'Error: {str(e)}')
         }
